b9c8eef6b388df99bc47096e73f12e21a8a0f0c6
amsali22 <aminefgx1@gmail.com>
2024-08-11 15:15:42 +0100

    2. Create Specific Components in a React Application
    2. Create Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import WelcomeMessage from './components/WelcomeMessage'
+import HeaderMessage from './components/Header'
+import FooterMessage from './components/Footer'
+import MainContent from './components/MainContent'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +22,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <WelcomeMessage />
+      <HeaderMessage />
+      <MainContent />
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
@@ -30,6 +35,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <FooterMessage />
     </>
   )
 }
---
+++ alx-react-app/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function FooterMessage() {
+  return (
+    <footer>
+        <p>© 2023 City Lovers</p>
+    </footer>
+  );
+}
+
+export default FooterMessage;
