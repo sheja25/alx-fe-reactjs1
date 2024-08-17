e670612bd5c3d2bea4fd22ff5e66e3a85366b89b
amsali22 <aminefgx1@gmail.com>
2024-08-17 23:24:32 +0100

    Update Navbar.jsx

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import {Link} from 'react-router-dom'
 
 const Navbar = () => {
     return (
-        <nav>
+        <nav style={{ backgroundColor: '#121212', display: 'block', justifyContent: 'center'}}>
            <ul>
             <li><Link to = '/'> Home </Link></li>
             <li><Link to = '/about'> About </Link></li>
