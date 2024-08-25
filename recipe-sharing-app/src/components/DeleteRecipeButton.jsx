d8d131a9d69ca53c1348bdef05cfbc38b081658b
amsali22 <aminefgx1@gmail.com>
2024-08-25 01:00:21 +0100

    2. Implementing Advanced Search and Filtering Capabilities
    2. Implementing Advanced Search and Filtering Capabilities

--- recipe-sharing-app/src/components/DeleteRecipeButton.jsx
+++ recipe-sharing-app/src/components/DeleteRecipeButton.jsx
@@ -1,13 +1,17 @@
+import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
+
 const DeleteRecipeButton = ({ recipeId }) => {
-  const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
-  const navigate = useNavigate();
+    const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+    const setSearchTerm = useRecipeStore(state => state.setSearchTerm)
+    const navigate = useNavigate();
 
   const handleDelete = () => {
     deleteRecipe(recipeId);
     navigate('/');
+    setSearchTerm('');
   };
 
   return <button onClick={handleDelete}>Delete Recipe</button>;
