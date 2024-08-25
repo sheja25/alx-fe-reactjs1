22d4e689aea3602d86374fbade9666eea617341a
amsali22 <aminefgx1@gmail.com>
2024-08-25 01:13:00 +0100

    3. Implementing User Favorites and Personalized Recipe Recommendations
    3. Implementing User Favorites and Personalized Recipe Recommendations

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -3,6 +3,7 @@ import RecipeList from './components/RecipeList';
 import AddRecipeForm from './components/AddRecipeForm';
 import RecipeDetails from './components/RecipeDetails';
 import SearchBar from './components/SearchBar';
+import Recommendations from './components/RecommendationsList';
 
 function App() {
  return (
@@ -11,6 +12,7 @@ function App() {
       <h1>Recipe Sharing App</h1>
       <SearchBar /> 
       <AddRecipeForm />
+      <Recommendations />
       <Routes>
         <Route path="/" element={<RecipeList />} />
         <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,11 +1,14 @@
 // RecipeDetails component
+import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 import EditRecipeForm from './EditRecipeForm';
 import DeleteRecipeButton from './DeleteRecipeButton';
+import FavBtn from './FavBtn';
 
 const RecipeDetails = () => {
     const { recipeId } = useParams(); 
+    const navigate = useNavigate();
     const recipe = useRecipeStore(state =>
       state.recipes.find(recipe => recipe.id === parseInt(recipeId))
     );
@@ -16,8 +19,12 @@ const RecipeDetails = () => {
     <div>
       <h1>{recipe.title}</h1>
       <p>{recipe.description}</p>
+      <FavBtn recipeId={recipe.id} />
       <EditRecipeForm recipe={recipe} />
       <DeleteRecipeButton recipeId={recipeId} />
+      <button onClick={() => navigate('/')} style={{ backgroundColor: 'blue', color: 'white', cursor: 'pointer' }}>
+        Back to Home
+      </button>
     </div>
   );
 };
