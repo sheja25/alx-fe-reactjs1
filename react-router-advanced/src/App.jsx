fbbea9709e02e217bc92edd63ea54002b72f0cb6
amsali22 <aminefgx1@gmail.com>
2024-08-31 22:19:17 +0100

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -9,41 +9,18 @@ import ProtectedRoute from './components/ProtectedRoute';
 function App() {
   return (
     <Routes>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/profile">Profile</Link>
-            </li>
-            <li>
-              <Link to="/profile/details">Profile Details</Link>
-            </li>
-            <li>
-              <Link to="/profile/settings">Profile Settings</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route  path="/">
-            <Home />
-          </Route>
+     <Routes>
+        <Route path= "/" element={<Home />} />
+        <Route path= "/profile" element={
           <ProtectedRoute>
            <Profile />
-          </ProtectedRoute>
-          <Route  path="/profile/details">
-            <ProfileDetails />
-          </Route>
-          <Route  path="/profile/settings">
-            <ProfileSettings />
-          </Route>
-          <Route  path="/blog/:id">
-            <BlogPost />
-          </Route>
-        </Switch>
-      </div>
+          </ProtectedRoute>}> 
+          <Route path= "details" element={<ProfileDetails />} />
+          <Route path= "settings" element={<ProfileSettings />} />
+        </ Route>  
+        <Route path='/blog/:id' element={<BlogPost />}/>
+        <Route path='/login' element={<Login />}/>
+     </Routes>
     </Routes>
   )
 }
