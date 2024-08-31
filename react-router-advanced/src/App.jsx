fb9a8399e806a27ff2ec344795abf4e57001d5e6
amsali22 <aminefgx1@gmail.com>
2024-08-31 22:12:47 +0100

    2. Advanced Routing in React with React Router

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,48 @@
+import './App.css'
+import { BrowserRouter as Router, Switch, Route, Link, Redirect } from 'react-router-dom';
+import Profile from './components/Profile';
+import ProfileDetails from './components/ProfileDetails';
+import ProfileSettings from './components/ProfileSettings';
+import BlogPost from './components/BlogPost';
+import ProtectedRoute from './components/ProtectedRoute';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/profile">Profile</Link>
+            </li>
+            <li>
+              <Link to="/profile/details">Profile Details</Link>
+            </li>
+            <li>
+              <Link to="/profile/settings">Profile Settings</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <ProtectedRoute>
+           <Profile />
+          </ProtectedRoute>
+          <Route exact path="/profile/details">
+            <ProfileDetails />
+          </Route>
+          <Route exact path="/profile/settings">
+            <ProfileSettings />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  )
+}
+
+export default App;
