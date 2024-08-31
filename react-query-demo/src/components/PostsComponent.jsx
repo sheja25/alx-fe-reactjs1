9aa7302127a7927e51887d6df7a8e1995f49a203
amsali22 <aminefgx1@gmail.com>
2024-08-31 21:57:43 +0100

    Update PostsComponent.jsx

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -32,3 +32,14 @@ const PostsComponent = () => {
 };
 
 export default PostsComponent;
+const fetchPosts = async () => {
+    const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+    if (!response.ok) {
+        throw new Error('Failed to fetch posts');
+    }
+    return response.json();
+};
+
+useEffect(() => {
+    fetchPosts();
+}, []);
